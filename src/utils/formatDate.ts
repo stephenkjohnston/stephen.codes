597fc35fde896e6aa027b9124e6ee91d9ba042e0
Stephen Johnston <stephenjohnston@protonmail.com>
2025-03-08 16:24:24 -0800

    Fix timezone in formateDate.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,13 +1,13 @@
 /**
  * Formats the date to the preferred format like "Jan 1, 2021"
- * @param date 
+ * @param date
  * @returns string "Jan 1, 2021"
  */
 export default function formateDate(date: Date): string {
-    return date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      timeZone: "UTC",
-    });
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: "PST",
+  });
 }
