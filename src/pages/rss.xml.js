3d3b92c6776ab017b788e5d56a2c46dd5ff3c4e5
Stephen Johnston <stephenjohnston@protonmail.com>
2025-05-22 10:21:14 -0700

    Fix RSS feed.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,17 +4,24 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
     const articles = await getCollection('articles');
+    const kilobits = await getCollection('kilobits');
+    const posts = [...articles, ...kilobits].sort((a, b) => {
+        return new Date(b.data.publishDate) - new Date(a.data.publishDate);
+    }).map((post) => ({
+        title: post.data.title,
+        pubDate: post.data.publishDate,
+        description: post.data.description,
+        collection: post.collection,
+        link: post.collection === 'articles' ? `/articles/${post.id}` : `/kilobits/${post.id}`,
+        customData: `<language>en-us</language>`,
+    }));
 
+    console.log({ posts });
     return rss({
         title: TITLE,
         description: DESCRIPTION,
         site: 'https://stephen.codes',
-        items: articles.map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.publishDate,
-            description: post.data.description,
-            link: `/articles/${post.slug}`
-        })),
+        items: posts,
         customData: `<language>en-us</language>`,
     });
 }
