61848692d7ce2c0edbba91e3690769e3f55c0b44
Stephen Johnston <stephenjohnston@protonmail.com>
2026-01-02 15:02:00 -0800

    Too many updates.

--- src/utils/getAllKilobitEntries.ts
+++ src/utils/getAllKilobitEntries.ts
@@ -4,5 +4,6 @@ export default async function getAllKilobitEntries() {
   const kilobitCollection = await getCollection("kilobits");
   return kilobitCollection.sort(
     (a, b) => b.data.publishDate.valueOf() - a.data.publishDate.valueOf()
-  );
+  )
+  .filter(article => article.data.status === "Publish");
 }
